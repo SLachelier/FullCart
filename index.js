18bd52d3335788f3c4619523d38f23dc0483624f
Shauna L <slachelier@gmail.com>
2024-01-30 08:46:50 -0600

    Cleared input field, added database, and initialized list
    Used firebase to push data from FullCart to a realtime database. Also added the features for clearing the input field after entering a value, and added capability for the list to populate with the entered values.

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"; //imports initializeApp function from firebase
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; //imports getDatabase function from firebase
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; //imports getDatabase function from firebase
 
 const appSettings = {
-    databaseURL: "https://full-cart-app-default-rtdb.firebaseio.com/" //database url
-}
+  databaseURL: "https://full-cart-app-default-rtdb.firebaseio.com/", //database url
+};
 
 const app = initializeApp(appSettings); //initializes firebase app
 const database = getDatabase(app); //gets database from firebase app
 const dbRef = ref(database); //gets reference to database
 
-let button = document.getElementById('add-button'); //obtains button element
+const inputFieldEl = document.getElementById("input-field");
+let button = document.getElementById("add-button"); //obtains button element
+const listEl = document.getElementById("shopping-list"); //obtains ul element
 
-button.addEventListener('click', function addItem(){ //executes function addItem() when button is clicked
-    let inputString = document.getElementById('input-field').value; //assigns the input value to inputString
-    
-    push(dbRef, inputString); //pushes inputString to the database
-})
+button.addEventListener("click", function addItem() {
+  //executes function addItem() when button is clicked
+  let inputString = inputFieldEl.value; //assigns the input value to inputString
+
+  push(dbRef, inputString); //pushes inputString to the database
+
+  listEl.innerHTML += `<li>${inputValue}</li>`;
+  //clears the input field when the button is clicked
+  inputFieldEl.value = "";
+});
